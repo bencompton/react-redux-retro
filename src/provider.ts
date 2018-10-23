199f690254e4f693861b28477fa2410ceb26c1e0
Compton <ben.compton@cerner.com>
2018-10-22 17:00:20 -0700

    initial commit

---
+++ src/provider.ts
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Provider } from 'react-redux';
+import { Store } from 'redux';
+
+export interface IProviderProps {
+    store: Store<any>;
+    actions: any;
+}
+
+export default (props: IProviderProps) => {
+  (props.store.dispatch as any).actions = props.actions;
+
+  return React.createElement(Provider, { store: props.store });
+};
